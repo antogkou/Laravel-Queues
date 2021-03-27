834b0b5de9310082ff9cd11fa8422ea6bdb3b5cd
Antonis Gkoutzamanis <antonis.gkoutzamanis@pfizer.com>
2021-03-27 14:41:49 +0200

    Initial commit
    Signed-off-by: Antonis Gkoutzamanis <antonis.gkoutzamanis@pfizer.com>

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/log', function () {
+    dispatch(function () {
+        logger('Hello there');
+    });
+
+    return 'Finished';
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
